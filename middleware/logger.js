9317983523f4218e769fdc6e3e5855f2ffd5a399
WAHOME <148408902+8WAHOME@users.noreply.github.com>
2025-10-23 05:38:35 +0300

    Add logger middleware to log request details

---
+++ middleware/logger.js
@@ -0,0 +1,7 @@
+// middleware/logger.js
+
+module.exports = (req, res, next) => {
+  const time = new Date().toISOString();
+  console.log(`[${time}] ${req.method} ${req.originalUrl}`);
+  next();
+};
